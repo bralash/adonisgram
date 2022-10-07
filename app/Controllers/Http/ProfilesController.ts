986cfb70c3e786996f1ed056e052cd7b14dff96b
bralash <emmanuelasaber@gmail.com>
2022-10-07 19:58:17 +0000

    Created uaer and post factory

--- app/Controllers/Http/ProfilesController.ts
+++ app/Controllers/Http/ProfilesController.ts
@@ -1,10 +1,12 @@
 import Application from "@ioc:Adonis/Core/Application";
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import User from "App/Models/User";
+import { UserFactory } from "Database/factories";
 
 export default class ProfilesController {
-  public async index({ view, params, auth }: HttpContextContract) {
+  public async index({ view, params }: HttpContextContract) {
     const user = await User.findBy("username", params.username);
+    // await UserFactory.with('posts',5).createMany(10)
     if (!user) {
       return view.render("errors/not-found", { title: "404 - Page not found" });
     }
@@ -14,7 +16,7 @@ export default class ProfilesController {
 
 
   public async edit({ view }: HttpContextContract) {
-    return view.render("accounts/edit");
+    return view.render("accounts/edit", {title: "Edit Profile"});
   }
 
   public async update({ auth, request, response }: HttpContextContract) {
--- database/factories/index.ts
+++ database/factories/index.ts
@@ -1,1 +1,27 @@
-// import Factory from '@ioc:Adonis/Lucid/Factory'
+import Factory from "@ioc:Adonis/Lucid/Factory";
+import Post from "App/Models/Post";
+import User from "App/Models/User";
+import { DateTime } from "luxon";
+
+export const UserFactory = Factory.define(User, ({ faker }) => {
+  return {
+    name: faker.name.fullName(),
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    password: faker.internet.password(),
+    bio: faker.lorem.sentences(),
+    email_verified_at: DateTime.local(),
+    avatar: faker.image.avatar(),
+  };
+})
+  .relation("posts", () => PostFactory)
+  .build();
+
+export const PostFactory = Factory.define(Post, ({ faker }) => {
+  return {
+    image: faker.image.animals(),
+    caption: faker.lorem.paragraph(),
+  };
+})
+  .relation("user", () => UserFactory)
+  .build();
